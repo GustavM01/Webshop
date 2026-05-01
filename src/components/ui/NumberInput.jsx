af67a315431820b58a0110ad1528e4874e0db19b
GustavM01 <gustav.milihagulu@gmail.com>
2026-05-01 18:04:13 +1000

    CSS cleanup

--- src/components/ui/NumberInput.jsx
+++ src/components/ui/NumberInput.jsx
@@ -6,19 +6,11 @@ function NumberInput({ value, onIncrease, onDecrease }) {
   const size = 16;
   return (
     <div className="input-container">
-      <button
-        disabled={value <= 1}
-        onClick={onDecrease}
-        className="input-btn decrease"
-      >
+      <button disabled={value <= 1} onClick={onDecrease} className="input-btn">
         <Minus color="white" size={size} />
       </button>
       <p>{value}</p>
-      <button
-        disabled={value >= 10}
-        onClick={onIncrease}
-        className="input-btn increase"
-      >
+      <button disabled={value >= 10} onClick={onIncrease} className="input-btn">
         <Plus color="white" size={size} />
       </button>
     </div>
